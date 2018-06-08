db5c13258951b6c7d26899f6377c4b18ac2c69dc
DataSecs <datasecs@outlook.de>
2018-06-08 02:26:01 +0200

    Started working on unit tests.

--- shared/src/test/java/de/datasecs/hydra/HydraSessionTest.java
+++ shared/src/test/java/de/datasecs/hydra/HydraSessionTest.java
@@ -1,8 +1,24 @@
 package de.datasecs.hydra;
 
+import de.datasecs.hydra.shared.handler.impl.HydraSession;
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created with love by DataSecs on 07.06.18
  */
 public class HydraSessionTest {
 
+    private HydraSession hydraSession;
+
+    @Test
+    public void checkConnection() {
+        Assert.assertTrue(hydraSession.isConnected());
+    }
+
+    @Test
+    public void checkClose() {
+        hydraSession.close();
+        Assert.assertFalse(hydraSession.getChannel().isOpen());
+    }
 }
